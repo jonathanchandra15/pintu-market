32d42f7dab2b1076fe68b2b81e1558498b392a72
jonathanchandra15 <jonathanjocan@gmail.com>
2024-12-06 23:34:05 +0700

    Create market page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import '@styles/Theme.scss'
 import 'primereact/resources/themes/lara-light-cyan/theme.css'
 import { Button } from 'primereact/button'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { Market } from '@views/market/Market'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Market />} />
+        <Route path="/" element={<Navigate to="/market" replace />} />
+        <Route path="/market" element={<Market />} />
       </Routes>
     </BrowserRouter>
   )
--- src/views/market/Market.tsx
+++ src/views/market/Market.tsx
@@ -1,3 +1,11 @@
+import { useMarket } from '@hooks/useMarket'
+import { useEffect } from 'react'
+
 export function Market() {
+  let { getSupportedCurrenciesRequest } = useMarket()
+
+  useEffect(() => {
+    getSupportedCurrenciesRequest()
+  }, [])
   return <p>A</p>
 }
