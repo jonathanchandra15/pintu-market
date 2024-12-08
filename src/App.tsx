58255a7aa477af13b5c65b43b72ea17322e75f21
jonathanchandra15 <jonathanjocan@gmail.com>
2024-12-08 15:50:29 +0700

    Add styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import '@styles/commons/Theme.scss'
 import '@styles/App.scss'
+import '@styles/commons/Table.scss'
 import 'primereact/resources/themes/lara-light-cyan/theme.css'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { Market } from '@views/market/Market'
@@ -11,6 +12,8 @@ import { CommonContext } from '@contexts/commonContext'
 
 import '@assets/fonts/NHaasGroteskTXPro-55Rg.ttf'
 import '@assets/fonts/NHaasGroteskTXPro-75Bd.ttf'
+import '@assets/fonts/Inter_28pt-Regular.ttf'
+import '@assets/fonts/Inter_24pt-Bold.ttf'
 
 const queryClient = new QueryClient()
 
@@ -20,10 +23,6 @@ function App() {
   function showErrorMessage(message: string) {
     showErrorToast(toastRef, message)
   }
-
-  useEffect(() => {
-    showErrorMessage('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
-  }, [])
 
   return (
     <QueryClientProvider client={queryClient}>
--- src/views/market/Market.tsx
+++ src/views/market/Market.tsx
@@ -6,60 +6,66 @@ import { convertNumberToIDRCurrency } from '@utils/commonFunction'
 import { CommonContext } from '@contexts/commonContext'
 import { CommonContextType } from '@utils/customTypes/contexts/commonContextCustomType'
 import React, { ReactElement } from 'react'
+import '@styles/market/Market.scss'
 
 export function Market(): ReactElement {
   const { showErrorMessage } = React.useContext(CommonContext) as CommonContextType
   let { supportedCurrencyList } = useMarket(showErrorMessage)
 
-  function generatePercentageBlock(number: string): ReactElement | string {
+  function generatePercentageBlock(number: string): ReactElement {
     let numberInFloat: number = parseFloat(number)
 
     if (numberInFloat > 0) {
       return (
-        <div>
-          <i className="pi pi-sort-up-fill" />
-          {number}%
+        <div className="l-table__row-value">
+          <i className="m-currency-table__percent-icon m-font--green pi pi-sort-up-fill" />
+          <p className="m-font--green m-font-inter--bold">{number}%</p>
         </div>
       )
     } else if (numberInFloat < 0) {
       return (
-        <div>
-          <i className="pi pi-sort-down-fill" />
-          {number}%
+        <div className="l-table__row-value">
+          <i className="m-currency-table__percent-icon m-font--red pi pi-sort-down-fill" />
+          <p className="m-font--red m-font-inter--bold">{number}%</p>
         </div>
       )
     }
-    return `${number}%`
+    return <p className="m-font--black m-font-inter--bold">{number}%</p>
   }
 
   return (
-    <div>
-      <h1>Crypto Price in Rupiah Today</h1>
+    <div className="l-market-page">
+      <div className="l-market-page__title">
+        <h1 className="m-font-neue">Crypto Price in Rupiah Today</h1>
+      </div>
       <DataTable
         value={supportedCurrencyList}
         tableStyle={{ minWidth: '50rem' }}
         removableSort
+        className="m-table m-currency-table m-font-inter"
+        scrollable
+        scrollHeight="80rem"
+        emptyMessage="No currency."
       >
         <Column
           field="currencyGroup"
-          body={(rowData) => {
-            return (
-              <div style={{ color: rowData.color }}>
-                {rowData.logo !== undefined && <ReactSVG src={rowData.logo} />}
-              </div>
-            )
-          }}
-        />
-        <Column
-          field="currencyGroup"
+          frozen
           header="CRYPTO"
           body={(rowData) => {
             return (
-              <div>
-                <p>
-                  <b>{rowData.name}</b>
-                </p>
-                <p>{rowData.currencyGroup}</p>
+              <div className="m-currency-table__currency">
+                <div
+                  className="m-currency-table__currency-icon"
+                  style={{ color: rowData.color }}
+                >
+                  <ReactSVG src={rowData.logo} className="m-currency-logo" />
+                </div>
+                <div className="m-currency-table__currency-name">
+                  <p className="m-font--black m-font-inter--bold">{rowData.name}</p>
+                  <p className="m-currency-table__currency-group m-font--grey">
+                    {rowData.currencyGroup}
+                  </p>
+                </div>
               </div>
             )
           }}
@@ -68,7 +74,11 @@ export function Market(): ReactElement {
           header="Price"
           field="latestPrice"
           body={(rowData) => {
-            return `${convertNumberToIDRCurrency(parseFloat(rowData.latestPrice))}`
+            return (
+              <p className="m-font--black m-font-inter--bold">
+                {convertNumberToIDRCurrency(parseFloat(rowData.latestPrice))}
+              </p>
+            )
           }}
           sortable
         />
