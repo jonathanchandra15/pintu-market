e5600e35960d95139f6df42a59aa56baa1a567c9
jonathanchandra15 <jonathanjocan@gmail.com>
2024-12-08 16:25:46 +0700

    Remove react-query library

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import '@styles/commons/Table.scss'
 import 'primereact/resources/themes/lara-light-cyan/theme.css'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { Market } from '@views/market/Market'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import 'primeicons/primeicons.css'
 import { MutableRefObject, useEffect, useRef, useState } from 'react'
 import { CustomToast, showErrorToast } from '@views/commons/CustomToast'
@@ -15,8 +14,6 @@ import '@assets/fonts/NHaasGroteskTXPro-75Bd.ttf'
 import '@assets/fonts/Inter_28pt-Regular.ttf'
 import '@assets/fonts/Inter_24pt-Bold.ttf'
 
-const queryClient = new QueryClient()
-
 function App() {
   const toastRef: MutableRefObject<any> = useRef(null)
 
@@ -25,21 +22,19 @@ function App() {
   }
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <CommonContext.Provider
-        value={{
-          showErrorMessage,
-        }}
-      >
-        <CustomToast toastRef={toastRef} />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Navigate to="/market" replace />} />
-            <Route path="/market" element={<Market />} />
-          </Routes>
-        </BrowserRouter>
-      </CommonContext.Provider>
-    </QueryClientProvider>
+    <CommonContext.Provider
+      value={{
+        showErrorMessage,
+      }}
+    >
+      <CustomToast toastRef={toastRef} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/market" replace />} />
+          <Route path="/market" element={<Market />} />
+        </Routes>
+      </BrowserRouter>
+    </CommonContext.Provider>
   )
 }
 
